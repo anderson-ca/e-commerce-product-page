2189b654c35a2e9f3555445a37c1102bf755b319
anderson <anderson.d.cardoso92@gmail.com>
2022-06-19 15:28:57 -0500

    add product display component

---
+++ components/ProductDisplay.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/ProductDisplay.module.css';
+
+const ProductDisplay = () => {
+  return (
+    <div className={styles.container}>
+    
+    </div>
+  )
+}
+
+export default ProductDisplay
